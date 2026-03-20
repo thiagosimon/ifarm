56a54bf9eb9d8dc249d7d7943ecfb0616ecbb5d6
thiagosimon <thiagosimon_15@hotmail.com>
2026-03-20 19:04:17 -0300

    fix: remove standalone output option from next.config.js

--- apps/web-retailer/next.config.js
+++ apps/web-retailer/next.config.js
@@ -3,7 +3,6 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   images: { domains: ["ifarm-assets.s3.sa-east-1.amazonaws.com"] },
 };
 module.exports = withNextIntl(nextConfig);
